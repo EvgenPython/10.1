eb48edf355dcde6c9c913c5da6ee57489e260096
keba_e <jekakeba@gmail.com>
2022-11-14 11:26:21 +0200

    v1.1

--- sup_family.py
+++ sup_family.py
@@ -1,14 +1,22 @@
 class Computer:
-    def calculate(self):
-        print("Calculating....")
+    def __init__(self, model, price,  *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.memory = 128
+        self.model = model
+        self.price = price
+
 
 class Display:
-    def display(self):
-        print("I display the image on the screen...")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.resolution = "4K"
 
 class SmartPhone(Display, Computer):
-    pass
+    def print_info(self):
+        print(self.resolution)
+        print(self.memory)
+        print(self.model)
+        print(self.price)
 
-iphone = SmartPhone()
-iphone.calculate()
-iphone.display()
+iphone = SmartPhone(model="Last", price = 1000)
+iphone.print_info()
